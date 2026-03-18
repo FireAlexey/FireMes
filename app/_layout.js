2e71d4ff7daaabb0cf4d8a49d81a8a036baf1552
FireAlexey <firealexey71@gmail.com>
2026-03-18 19:30:25 +0300

    ssgdkjdfa;

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,14 @@
 import { Slot } from 'expo-router'
-import { View } from 'react-native'
+import { View, Platform } from 'react-native'
+import { useEffect } from 'react'
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (Platform.OS === 'web') {
+      document.title = 'FireMes'
+    }
+  }, [])
+
   return (
     <View style={{ flex: 1 }}>
       <Slot />
